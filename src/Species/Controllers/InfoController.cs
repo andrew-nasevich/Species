20186ff23049232c7f888271d563a29ec5e53e10
Andrew <nasevichandrey@gmail.com>
2021-05-10 23:06:40 +0300

    Add ProtectionСategories page

---
+++ src/Species/Controllers/InfoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Species.Controllers
+{
+    [AllowAnonymous]
+    public class InfoController : Controller
+    {
+        public IActionResult ProtectionСategories()
+        {
+            return View();
+        }
+    }
+}
